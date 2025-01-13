f83ba6141c60255c97bbdde889925adc4f327dab
Yom-Coding <yompatel88888888@gmail.com>
2025-01-13 18:06:27 +0000

    Add files via upload

---
+++ Crescent_Moon HW.py
@@ -0,0 +1,34 @@
+import turtle 
+screen = turtle.Screen()
+turtle.speed(0)
+screen.bgcolor("dark blue")
+
+turtle.penup()
+turtle.goto(-4,-324)
+turtle.color("orange")
+
+turtle.begin_fill()
+turtle.circle(350)
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(-4,-104)
+turtle.color("dark blue")
+
+turtle.begin_fill()
+turtle.circle(350)
+turtle.end_fill()
+
+screen.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
